8a59a0de957b1028f78f7ba1a5152aa5658edf7d
adriguerre <adrianguerreroalvarez@gmail.com>
2022-03-15 12:19:50 +0100

    Update DoubleLinkedListQueueTest.java

--- src/test/java/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleLinkedListQueueTest.java
@@ -6,4 +6,23 @@ import org.junit.jupiter.api.Test;
 
 class DoubleLinkedListQueueTest {
 
+    private DoubleLinkedListQueue list;
+
+    @BeforeEach
+    public void setup(){
+        list = new DoubleLinkedListQueue();
+    }
+
+    @AfterEach
+    public void finish(){
+        list = null;
+    }
+
+    @Test
+    public void testComputeReturnSizeBiggerThanPreviousSizeWhenAppend(){
+        list.append(new DequeNode(5, null, null));
+        list.append(new DequeNode(3, null, null));
+
+
+    }
 }
