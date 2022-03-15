b96b00723801fd26f90d6583d81e545698cee22e
AndresLP01 <galoan2001@uma.es>
2022-03-15 21:51:00 +0100

    Added last testcases

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,10 +1,7 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Deque;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DequeNodeTest<T> {
     DequeNode<T> node,previous,next;
@@ -79,6 +76,39 @@ public class DequeNodeTest<T> {
         boolean valorObtenido = node.isLastNode();
 
         assertEquals(valorEsperado,valorObtenido);
+    }
 
+    @Test
+    public void testReturnTrueIfPreviousAndNextAreNull(){
+        var nodePrev = new DequeNode<>(null,null,null);
+        var nodePost = new DequeNode<>(null, null, null);
+        node.setNext((DequeNode<T>) nodePost);
+        node.setPrevious((DequeNode<T>) nodePrev);
+
+        assertTrue(node.isNotATerminalNode());
+    }
+
+    @Test
+    public void testSetNextMustChangeNextNode(){
+        assertTrue(node.getNext() == null);
+        var node1 = new DequeNode<>(null,null,null);
+        node.setNext((DequeNode<T>) node1);
+        assertTrue(node.getNext()!=null && node.getNext()== node1);
+    }
+    @Test
+    public void testSetItemMustChangeNodeItem(){
+        T itemPrev = node.getItem();
+        assertEquals(itemPrev, node.getItem());
+        T itemPost = null;
+        node.setItem(itemPost);
+        assertEquals(itemPost,node.getItem());
+    }
+
+    @Test
+    public void testSetPreviousMustChangePrevious() {
+        assertTrue(node.getPrevious() == null);
+        var node1 = new DequeNode<>(null, null, null);
+        node.setPrevious((DequeNode<T>) node1);
+        assertTrue(node.getPrevious() != null && node.getPrevious() == node1);
     }
 }
