92d3bb36dca5767aa3b62ebed74ad42e87bba257
adriguerre <adrianguerreroalvarez@gmail.com>
2022-03-22 12:04:08 +0100

    Rebase 2

--- src/test/java/ComplexOperationsTest.java
+++ src/test/java/ComplexOperationsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ComplexOperationsTest {
-    DoubleLinkedListQueue empty, list1, list2, duplicated;
+    DoubleLinkedListQueue empty, list1, list2;
 
 
     @BeforeEach
@@ -14,7 +14,6 @@ public class ComplexOperationsTest {
         this.empty = new DoubleLinkedListQueue<>();
         this.list1 = new DoubleLinkedListQueue<>();
         this.list2 = new DoubleLinkedListQueue<>();
-        this.duplicated = new DoubleLinkedListQueue<>();
 
         list1.append(new DequeNode(5, null, null));
         list2.append(new DequeNode(5, null, null));
@@ -22,9 +21,6 @@ public class ComplexOperationsTest {
         list2.append(new DequeNode(6, null, null));
         list2.append(new DequeNode(9, null, null));
         list2.append(new DequeNode(12, null, null));
-
-        duplicated.append(new DequeNode(2,null,null));
-        duplicated.append(new DequeNode(2,null,null));
     }
 
     @AfterEach
@@ -34,97 +30,46 @@ public class ComplexOperationsTest {
         this.list2 = null;
     }
 
-    /*
-        -------- getAt Testing --------
-     */
 
     /*
-        Scenario: getAt con un índice mayor que el tamaño
-     */
+        Eliminar un elemento de una lista vacia
+    */
 
     @Test
-    public void shouldReturnErrorIfIndexOutOfBounds(){
-        assertThrows(DoubleLinkedListQueueException.class, ()->empty.getAt(1));
-        assertThrows(DoubleLinkedListQueueException.class, ()->list1.getAt(1));
-        assertThrows(DoubleLinkedListQueueException.class, ()->list2.getAt(list1.size()+1));
-    }
-    /*
-        Scenario: getAt recibe un indice negativo
-     */
-    @Test
-    public void shouldReturnErrorIfIndexIsNegative(){
-        assertThrows(DoubleLinkedListQueueException.class, ()->empty.getAt(-1));
-        assertThrows(DoubleLinkedListQueueException.class, ()->list1.getAt(-5));
-        assertThrows(DoubleLinkedListQueueException.class, ()->list2.getAt(-20));
-    }
-    /*
-        Scenario: getAt devuelve un item null
-     */
-    @Test
-    public void shouldReturnNullIfElementAtIndexIsNull(){
-        var node = new DequeNode<>(null,null,null);
-        list1.append(node); //Index at 1
-        assertEquals(null,list1.getAt(1));
+    public void testDeleteReturnExceptionWhenDeletingElementFromEmptyList(){
+        assertThrows(DoubleLinkedListQueueException.class, () -> empty.delete(new DequeNode(5, null, null)));
     }
 
     /*
-        Scenario: getAt en una lista vacía
-     */
-
+        Intentar eliminar un elemento que no esta en la lista
+    */
     @Test
-    public void shouldReturnErrorIfGetAtIfCalledInEmptyList(){
-        assertThrows(DoubleLinkedListQueueException.class, ()->empty.getAt(1));
-    }
-    /*
-        Scenario: getAt en una lista no vacía devuelve el elemento correspondiente
-     */
-
-
-    @Test
-    public void shouldReturnCorrectElementAtIndex(){
-        int expectedValue = 5;
-        assertEquals(expectedValue, list1.getAt(0));
+    public void testDeleteReturnExceptionWhenTryingToDeleteELementThatIsNotInTheList(){
+        assertThrows(DoubleLinkedListQueueException.class, () -> list1.delete(new DequeNode(9, null, null)));
     }
 
     /*
-        Scenario: find a una lista vacía
+        Eliminar un elemento dentro de una lista con 5 elementos
      */
 
     @Test
-    public void shouldReturnErrorIfFindIsCalledInEmptyList(){
-        assertThrows(DoubleLinkedListQueueException.class, ()->empty.find(null));
+    public void testDeleteReturnTrueWhenDeletingElementFromList(){
+        int expectedValue = list2.size() - 1;
+        list2.delete(list2.find(6));
+        int obtainedValue = list2.size();
+
+        assertTrue(list2.find(6)==null);
     }
 
     /*
-        Scenario: find de un elemento que no está en la lista
+        Eliminar el primer elemento de una lista no vacia
      */
 
     @Test
-    public void shouldReturnErrorIfElementNotInList(){
-        assertThrows(DoubleLinkedListQueueException.class, ()-> list1.find(90));
-    }
+    public void testDeleteFirstInAList(){
+        DequeNode second = list2.peekFirst().getNext();
+        list2.delete(list2.peekFirst());
 
-    /*
-        Scenario: find de un elemento que está en la lista
-     */
-    @Test
-    public void shouldReturnCorrectNode(){ //IDK
-        var expectedNode1 = new DequeNode<>(5,null,null);
-        assertEquals(expectedNode1, list1.find(5));
-
-        var obtainedNode = list2.find(9);
-
-        var expectedNode2 = new DequeNode<>(9,DequeNode.class,DequeNode.class);
-
-        assertEquals(expectedNode2,obtainedNode);
-    }
-
-    /*
-        Scenario: find de un elemento repetido en la lista
-     */
-    @Test
-    public void shouldReturnOneNodeIfElementIfDuplicated(){
-        var auxNext = new DequeNode<>(2,null,null);
-        var expectedNode = new DequeNode<>(2, auxNext,null);
+        assertTrue(second.equals(list2.peekFirst()));
     }
 }
