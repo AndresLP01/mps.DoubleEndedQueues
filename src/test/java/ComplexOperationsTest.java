70d10f9dafa8ce91e9955211308a15b020009448
adriguerre <adrianguerreroalvarez@gmail.com>
2022-03-22 12:04:08 +0100

    Added delete test

--- src/test/java/ComplexOperationsTest.java
+++ src/test/java/ComplexOperationsTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ComplexOperationsTest {
@@ -26,5 +28,48 @@ public class ComplexOperationsTest {
         this.empty = null;
         this.list1 = null;
         this.list2 = null;
+    }
+
+
+    /*
+        Eliminar un elemento de una lista vacia
+    */
+
+    @Test
+    public void testDeleteReturnExceptionWhenDeletingElementFromEmptyList(){
+        assertThrows(DoubleLinkedListQueueException.class, () -> empty.delete(new DequeNode(5, null, null)));
+    }
+
+    /*
+        Intentar eliminar un elemento que no esta en la lista
+    */
+    @Test
+    public void testDeleteReturnExceptionWhenTryingToDeleteELementThatIsNotInTheList(){
+        assertThrows(DoubleLinkedListQueueException.class, () -> list1.delete(new DequeNode(9, null, null)));
+    }
+
+    /*
+        Eliminar un elemento dentro de una lista con 5 elementos
+     */
+
+    @Test
+    public void testDeleteReturnTrueWhenDeletingElementFromList(){
+        int expectedValue = list2.size() - 1;
+        list2.delete(list2.find(6));
+        int obtainedValue = list2.size();
+
+        assertTrue(list2.find(6)==null);
+    }
+
+    /*
+        Eliminar el primer elemento de una lista no vacia
+     */
+
+    @Test
+    public void testDeleteFirstInAList(){
+        DequeNode second = list2.peekFirst().getNext();
+        list2.delete(list2.peekFirst());
+
+        assertTrue(second.equals(list2.peekFirst()));
     }
 }
